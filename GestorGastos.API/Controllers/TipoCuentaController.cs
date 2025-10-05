47528bcc22468cc26aa18f657e5f0550e4e0c424
Rojak <namor.ku@gmail.com>
2025-10-05 02:42:12 -0600

    chore: agrega .gitignore y proyecto inicial

---
+++ GestorGastos.API/Controllers/TipoCuentaController.cs
@@ -0,0 +1,36 @@
+﻿using GestorGastos.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorGastos.API.Controllers
+{
+    [Route("api/tipoCuenta")]
+    [ApiController]
+    public class TipoCuentaController : ControllerBase //para obtener metodos auxiliares pata trabajar con webapi
+    {
+        private readonly IConfiguration configuration;
+
+        public TipoCuentaController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public List<TipoCuenta> Get()
+        {
+            return [];
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TipoCuenta> Get(int id)
+        {
+            var tipoCuenta = new TipoCuenta() { Nombre = "shshshs" };
+            if (tipoCuenta is null)
+            {
+                return NotFound(); //404 recurso no encontrado
+            }
+
+            return tipoCuenta;
+        }
+
+    }
+}
---
+++ GestorGastos.API/Entidades/TipoCuenta.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorGastos.API.Entidades
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")] //net.core para validar en API
+        [StringLength(50, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+        public required string Nombre { get; set; }
+    }
+}
